e43d2bdaaec68ab920d46bd867ba8ec91145d39d
Szer13 <shad350@gmail.com>
2023-09-11 20:41:18 +0200

    encja Article

---
+++ src/main/java/pl/coderslab/model/Article.java
@@ -0,0 +1,90 @@
+package pl.coderslab.model;
+
+
+import org.hibernate.mapping.ToOne;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Entity
+@Table(name = "articles")
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 200)
+    private String title; // (max. 200 znaków)
+    @ManyToOne
+    private Author author; // (powiązanie relacją do klasy Author) - artykuł może mieć tylko jednego autora
+    @OneToMany
+    private List<Category> categories; // (powiązanie relacją do klasy Category) - artykuł może należeć do wielu kategorii
+    private String content;
+    private String created = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")); // (wartość ma być automatycznie dodawana podczas zapisu)
+    private String updated = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")); // (wartość ma być automatycznie zmieniana podczas edycji).
+
+    public Article(){
+        
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author=" + author +
+                ", categories=" + categories +
+                ", content='" + content + '\'' +
+                ", created='" + created + '\'' +
+                ", updated='" + updated + '\'' +
+                '}';
+    }
+}
