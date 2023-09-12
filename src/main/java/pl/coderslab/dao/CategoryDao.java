364d02bbef6c8fb3d2f258d7014e8cc354fb1cb4
Szer13 <shad350@gmail.com>
2023-09-12 21:40:47 +0200

    all dao's find all and find by id

--- src/main/java/pl/coderslab/dao/CategoryDao.java
+++ src/main/java/pl/coderslab/dao/CategoryDao.java
@@ -6,6 +6,7 @@ import pl.coderslab.model.Category;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
@@ -14,15 +15,23 @@ public class CategoryDao {
     @PersistenceContext
     private EntityManager entityManager;
 
-    public void saveCategory(Category category){
+    public void saveCategory(Category category) {
         entityManager.persist(category);
     }
 
-    public void deleteCategory(Category category){
+    public void deleteCategory(Category category) {
         entityManager.remove(entityManager.contains(category) ? category : entityManager.merge(category));
     }
 
-    public void updateCategory(Category category){
+    public void updateCategory(Category category) {
         entityManager.merge(category);
+    }
+
+    public Category findCategoryById(Long id){
+        return entityManager.find(Category.class, id);
+    }
+
+    public List<Category> findAllCategories(){
+        return entityManager.createQuery("SELECT c FROM Category c", Category.class).getResultList();
     }
 }
